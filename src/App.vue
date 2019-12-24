59d1e04012d90a1db9b39918861a60f6316e7c0e
junwei0117 <v860117@gmail.com>
2019-12-24 11:30:57 +0800

    Fix the IRI endpoint

--- src/App.vue
+++ src/App.vue
@@ -4,43 +4,51 @@
       <nav class="navbar">
         <div class="navbar-brand">
           <router-link :to="{ name: 'Home' }" class="navbar-item">
-            <img src="./assets/logo.png" alt="logo" width="28" height="28" style="margin-right: 6px">
+            <img
+              src="./assets/logo.png"
+              alt="logo"
+              width="28"
+              height="28"
+              style="margin-right: 6px"
+            />
             <h1 class="heading title"></h1>
             <h1 class="heading title is-4">Open IOTA</h1>
           </router-link>
-          <div class="navbar-burger" v-bind:class="{ 'is-active': navVisible }" @click="navVisible = !navVisible">
+          <div
+            class="navbar-burger"
+            v-bind:class="{ 'is-active': navVisible }"
+            @click="navVisible = !navVisible"
+          >
             <span></span>
             <span></span>
             <span></span>
           </div>
         </div>
         <div class="navbar-menu" v-bind:class="{ 'is-active': navVisible }">
-          <div class="navbar-start">
-
-          </div>
+          <div class="navbar-start"></div>
           <div class="navbar-end">
-
-            <div class="navbar-item animated" v-bind:class="{'fadeInDown': priceUSD !== 0}" style="opacity: 0">
-              <a href="https://www.cryptocompare.com/coins/iot/overview/USD" class="button is-info" target="_blank">
-                ${{priceUSD}}/Mi
-              </a>
-            </div>
-
             <div class="navbar-item">
               <b-dropdown @change="connectToIOTA" v-model="iota.provider" position="is-bottom-left">
-
-                <button type="button" slot="trigger" class="button" :class="{
+                <button
+                  type="button"
+                  slot="trigger"
+                  class="button"
+                  :class="{
                     'is-loading': iota.status === 'Connecting',
                     'is-primary': iota.status === 'Connected',
-                    'is-danger': iota.status === 'Failed'}">
+                    'is-danger': iota.status === 'Failed'}"
+                >
                   <span class="icon is-small">
-                    <i class="fa" :class="{
+                    <i
+                      class="fa"
+                      :class="{
                     'fa-wifi': iota.status === 'Connected',
                     'fas fa-exclamation-triangle': iota.status === 'Failed',
                     'fa-spinner fa-pulse': iota.status === 'Connecting'
-                    }"></i>
+                    }"
+                    ></i>
                   </span>
-                  <span> {{ this.iota.status }} </span>
+                  <span>{{ this.iota.status }}</span>
                   <b-icon icon="arrow_drop_down"></b-icon>
                 </button>
 
@@ -56,19 +64,29 @@
                 <b-dropdown-item custom>
                   <h1 class="title is-6">Latest Solid Milestone:</h1>
                   <b-field class="subtitle">
-                    <b-input expanded spellcheck="false" readonly :value="iota.latestSolidMilestone"></b-input>
+                    <b-input
+                      expanded
+                      spellcheck="false"
+                      readonly
+                      :value="iota.latestSolidMilestone"
+                    ></b-input>
                     <p class="control">
-                      <button class="button is-primary" v-clipboard:copy="iota.latestSolidMilestone">Copy</button>
+                      <button
+                        class="button is-primary"
+                        v-clipboard:copy="iota.latestSolidMilestone"
+                      >Copy</button>
                     </p>
                   </b-field>
                 </b-dropdown-item>
                 <b-dropdown-item custom>
-                  Node version: <b>{{ iota.version || "..." }}</b>
+                  Node version:
+                  <b>{{ iota.version || "..." }}</b>
                 </b-dropdown-item>
                 <b-dropdown-item custom>
-                  Node health: <b>{{ providerHealth }}</b>
+                  Node health:
+                  <b>{{ providerHealth }}</b>
                 </b-dropdown-item>
-                <hr class="dropdown-divider">
+                <hr class="dropdown-divider" />
                 <div v-for="prov in providerList">
                   <b-dropdown-item :value="prov">
                     <div class="media">
@@ -90,27 +108,7 @@
                   </b-field>
                 </b-dropdown-item>
               </b-dropdown>
-
             </div>
-
-            <div class="navbar-item" >
-              <a href="http://itsfe.prizziota.com/" class="button is-success" target="_blank">
-              <span class="icon">
-                <i class="fas fa-exchange-alt"></i>
-              </span>
-                <span>Live Transactions</span>
-              </a>
-            </div>
-
-            <div class="navbar-item" >
-              <a href="https://github.com/pRizz/open-iota" class="button" target="_blank">
-              <span class="icon">
-                <i class="fab fa-github"></i>
-              </span>
-                <span>GitHub</span>
-              </a>
-            </div>
-
           </div>
         </div>
       </nav>
@@ -119,54 +117,21 @@
     <router-view :iota="iota.link"></router-view>
 
     <!-- Open IOTA Ad -->
-    <ins class="adsbygoogle"
-         style="display:block"
-         data-ad-client="ca-pub-5014469443290772"
-         data-ad-slot="8143969435"
-         data-ad-format="auto"
-         data-full-width-responsive="true"></ins>
-    
+    <ins
+      class="adsbygoogle"
+      style="display:block"
+      data-ad-client="ca-pub-5014469443290772"
+      data-ad-slot="8143969435"
+      data-ad-format="auto"
+      data-full-width-responsive="true"
+    ></ins>
+
     <footer class="footer">
       <div class="container">
         <div class="columns">
           <div class="column">
             <h1 class="heading title is-4">Open IOTA</h1>
             <h1 class="subtitle is-6">A simple, free, open-source tangle explorer for IOTA</h1>
-          </div>
-          <div class="column">
-            <p>
-              For more information, view the README on
-              <a class="button is-small" href="https://github.com/pRizz/open-iota/blob/master/README.md" target="_blank">
-                  <span class="icon is-small">
-                    <i class="fab fa-github"></i>
-                  </span>
-                <span>GitHub</span>
-              </a>
-            </p>
-            <p>
-              This site is hosted by <a target="_blank" href="https://www.prizzventuresllc.com/">P Rizz Ventures LLC</a>
-            </p>
-            <p>
-              <a href="https://open-iota.prizziota.com/PrivacyPolicy.txt" target="_blank">Privacy Policy</a>
-            </p>
-          </div>
-          <div class="column">
-            <p>Created by Laurence Squires
-              <a class="button is-small" href="https://github.com/lsquires/open-iota" target="_blank">
-                <span class="icon is-small">
-                  <i class="fab fa-github"></i>
-                </span>
-                <span>GitHub</span>
-              </a>
-            </p>
-            <p>Forked by Peter Ryszkiewicz
-              <a class="button is-small" href="https://github.com/pRizz/open-iota" target="_blank">
-                <span class="icon is-small">
-                  <i class="fab fa-github"></i>
-                </span>
-                <span>GitHub</span>
-              </a>
-            </p>
           </div>
         </div>
       </div>
@@ -179,12 +144,14 @@
           <button class="delete" aria-label="close" @click="closeModal"></button>
         </header>
         <section class="modal-card-body">
+          <div>Doing this will open this website in another tab.</div>
+          <br />
           <div>
-            Doing this will open this website in another tab.
-          </div>
-          <br/>
-          <div>
-            The technical reason for this is that when you interact with an <code>http</code> server, but the website itself is served over <code>https</code>, the requests will be blocked by the web browser. (This is a security feature built into web browsers.) Therefore, you must go to the <code>http</code> version of this website to interact with <code>http</code> IOTA servers.
+            The technical reason for this is that when you interact with an
+            <code>http</code> server, but the website itself is served over
+            <code>https</code>, the requests will be blocked by the web browser. (This is a security feature built into web browsers.) Therefore, you must go to the
+            <code>http</code> version of this website to interact with
+            <code>http</code> IOTA servers.
           </div>
         </section>
         <footer class="modal-card-foot">
@@ -193,140 +160,130 @@
         </footer>
       </div>
     </b-modal>
-
   </div>
 </template>
 
 <script>
-  import IOTA from 'iota.lib.js'
-  import ValueHelper from './components/mixins/ValuesHelper'
-  import BModal from 'buefy/src/components/modal/Modal'
-  import curlTransaction from 'curl-transaction-core'
-  import curlImpl from 'curl-transaction-webgl2-impl'
+import IOTA from "iota.lib.js";
+import ValueHelper from "./components/mixins/ValuesHelper";
+import BModal from "buefy/src/components/modal/Modal";
+import curlTransaction from "curl-transaction-core";
+import curlImpl from "curl-transaction-webgl2-impl";
 
-  // public node list from IOTA's mainnet nodes, https://iotasupport.com/providers.json, and http://iotanode.host/
-  const defaultProviderList = [
-    // http nodes
-    'http://eugene.iota.community:14265',
-    'http://node01.iotatoken.nl:14265',
-    'http://node02.iotatoken.nl:14265',
-    'http://node03.iotatoken.nl:14265',
-    'http://mainnet.necropaz.com:14500',
-    'http://node.lukaseder.de:14265',
-    'http://iota-node-nelson.prizziota.com:80',
-    // https nodes
-    'https://iota-node-nelson.prizziota.com:443',
-    'https://iotanode.us:443',
-    'https://iri2-api.iota.fm:443',
-    'https://iri3-api.iota.fm:443',
-    'https://nelson1-api.iota.fm:443',
-    'https://node.iota-community.org:443',
-    'https://node.iota.dance:443',
-    'https://node.neffware.com:443',
-    'https://node1.iotaner.org:443',
-    'https://nodes.iota.cafe:443',
-    'https://wallet1.iota.town:443',
-    'https://wallet2.iota.town:443'
-  ]
+// public node list from IOTA's mainnet nodes, https://iotasupport.com/providers.json, and http://iotanode.host/
+const defaultProviderList = [
+  // http nodes
+  "http://node1.puyuma.org:14265"
+];
 
-  const initialProvider = 'https://iota-node-nelson.prizziota.com:443'
+const initialProvider = "http://node1.puyuma.org:14265";
 
-  let localAttachToTangle = null
+let localAttachToTangle = null;
 
-  export default {
-    components: {BModal},
-    name: 'app',
-    mixins: [ValueHelper],
-    data () {
-      return {
-        navVisible: false,
-        customProvider: '',
-        donationAddress: 'Coming soon...',
-        providerList: defaultProviderList,
-        iota: {
-          status: '',
-          link: null,
-          provider: initialProvider,
-          connected: false,
-          latestMilestone: '',
-          latestSolidMilestone: '',
-          latestMilestoneIndex: 0,
-          latestSolidSubtangleMilestoneIndex: 0
-        },
-        isProviderModalActive: false,
-        intendedHTTPSite: null
-      }
-    },
-    computed: {
-      providerHealth () {
-        return this.iota.latestSolidSubtangleMilestoneIndex + ' / ' + this.iota.latestMilestoneIndex
-      }
-    },
-    methods: {
-      addProvider () {
-        if(this.customProvider) {
-          this.providerList.push(this.customProvider)
-          this.customProvider = ''
-        }
+export default {
+  components: { BModal },
+  name: "app",
+  mixins: [ValueHelper],
+  data() {
+    return {
+      navVisible: false,
+      customProvider: "",
+      donationAddress: "Coming soon...",
+      providerList: defaultProviderList,
+      iota: {
+        status: "",
+        link: null,
+        provider: initialProvider,
+        connected: false,
+        latestMilestone: "",
+        latestSolidMilestone: "",
+        latestMilestoneIndex: 0,
+        latestSolidSubtangleMilestoneIndex: 0
       },
-      connectToIOTA () {
-        // if(window.location.protocol.includes('http:')) {
-        if(window.location.protocol.includes('https:') && this.iota.provider.includes('http:')) {
-          this.intendedHTTPSite = this.iota.provider
-          this.isProviderModalActive = true
-          // FIXME: this.iota.provider is stale
-          // this.iota.provider = this.iota.link.provider ?
-          return
-        }
-
-        this.iota.status = 'Connecting' // TODO: rename this.iota to this.iotaConnectionController
-        this.iota.connected = false
-        this.iota.latestMilestoneIndex = 0
-        this.iota.latestSolidSubtangleMilestoneIndex = 0
-        this.iota.version = null
-
-        this.iota.link = new IOTA({
-          provider: this.iota.provider
-        })
-
-        this.iota.link.api.attachToTangle = localAttachToTangle
-
-        let currentLink = this.iota.link
-
-        this.iota.link.api.getNodeInfo((err, success) => {
-          if(currentLink !== this.iota.link) { return }
-          if (err) {
-            this.iota.status = 'Failed'
-            return
-          }
-          this.iota.status = 'Connected'
-          this.iota.connected = true
-          this.iota.latestMilestone = success.latestMilestone
-          this.iota.latestSolidMilestone = success.latestSolidSubtangleMilestone
-          this.iota.latestMilestoneIndex = success.latestMilestoneIndex
-          this.iota.latestSolidSubtangleMilestoneIndex = success.latestSolidSubtangleMilestoneIndex
-          this.iota.version = success.appVersion
-        })
-      },
-      openHTTPSite() {
-        this.isProviderModalActive = false
-        window.open(window.location.href.replace('https:', 'http:'), '_blank')
-      },
-      closeModal() {
-        this.isProviderModalActive = false
-      }
-    },
-    mounted () {
-      if(!curlImpl.error) {
-        localAttachToTangle = curlTransaction({ curlImpl }).localAttachToTangle
-        console.log('Your browser does support WebGL2')
-      } else {
-        console.error('Your browser does not support WebGL2')
-        this.isWebGL2Supported = false
-      }
-
-      this.connectToIOTA()
+      isProviderModalActive: false,
+      intendedHTTPSite: null
+    };
+  },
+  computed: {
+    providerHealth() {
+      return (
+        this.iota.latestSolidSubtangleMilestoneIndex +
+        " / " +
+        this.iota.latestMilestoneIndex
+      );
     }
+  },
+  methods: {
+    addProvider() {
+      if (this.customProvider) {
+        this.providerList.push(this.customProvider);
+        this.customProvider = "";
+      }
+    },
+    connectToIOTA() {
+      // if(window.location.protocol.includes('http:')) {
+      if (
+        window.location.protocol.includes("https:") &&
+        this.iota.provider.includes("http:")
+      ) {
+        this.intendedHTTPSite = this.iota.provider;
+        this.isProviderModalActive = true;
+        // FIXME: this.iota.provider is stale
+        // this.iota.provider = this.iota.link.provider ?
+        return;
+      }
+
+      this.iota.status = "Connecting"; // TODO: rename this.iota to this.iotaConnectionController
+      this.iota.connected = false;
+      this.iota.latestMilestoneIndex = 0;
+      this.iota.latestSolidSubtangleMilestoneIndex = 0;
+      this.iota.version = null;
+
+      this.iota.link = new IOTA({
+        provider: this.iota.provider
+      });
+
+      this.iota.link.api.attachToTangle = localAttachToTangle;
+
+      let currentLink = this.iota.link;
+
+      this.iota.link.api.getNodeInfo((err, success) => {
+        if (currentLink !== this.iota.link) {
+          return;
+        }
+        if (err) {
+          this.iota.status = "Failed";
+          return;
+        }
+        this.iota.status = "Connected";
+        this.iota.connected = true;
+        this.iota.latestMilestone = success.latestMilestone;
+        this.iota.latestSolidMilestone = success.latestSolidSubtangleMilestone;
+        this.iota.latestMilestoneIndex = success.latestMilestoneIndex;
+        this.iota.latestSolidSubtangleMilestoneIndex =
+          success.latestSolidSubtangleMilestoneIndex;
+        this.iota.version = success.appVersion;
+      });
+    },
+    openHTTPSite() {
+      this.isProviderModalActive = false;
+      window.open(window.location.href.replace("https:", "http:"), "_blank");
+    },
+    closeModal() {
+      this.isProviderModalActive = false;
+    }
+  },
+  mounted() {
+    if (!curlImpl.error) {
+      localAttachToTangle = curlTransaction({ curlImpl }).localAttachToTangle;
+      console.log("Your browser does support WebGL2");
+    } else {
+      console.error("Your browser does not support WebGL2");
+      this.isWebGL2Supported = false;
+    }
+
+    this.connectToIOTA();
   }
+};
 </script>
 
